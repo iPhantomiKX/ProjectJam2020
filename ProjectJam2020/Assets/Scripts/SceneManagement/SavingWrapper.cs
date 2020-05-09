2afbfe02fbc6b51c0330f1a330c10de360b2bddd
iPhantomiKX <cmpsun98@gmail.com>
2020-05-09 23:22:31 +0800

    Added Zombie Feature Added basic camera controls & combat mechanics
    - Amirul 09052020

---
+++ ProjectJam2020/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        [SerializeField] KeyCode saveKey = KeyCode.S;
+        [SerializeField] KeyCode loadKey = KeyCode.L;
+        [SerializeField] KeyCode deleteKey = KeyCode.Delete;
+        const string defaultSaveFile = "save";
+        [SerializeField] float fadeInTime = 0.5f;
+
+        private void Awake()
+        {
+            StartCoroutine(LoadLastScene());
+        }
+
+        IEnumerator LoadLastScene()
+        {
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+
+            Fader fader = FindObjectOfType<Fader>();
+
+            fader.FadeOutImmediate();
+            yield return fader.FadeIn(fadeInTime);
+        }
+
+        void Update()
+        {
+            if(Input.GetKeyDown(loadKey))
+            {
+                Load();
+            }
+
+            if (Input.GetKeyDown(saveKey))
+            {
+                Save();
+            }
+
+            if (Input.GetKeyDown(deleteKey))
+            {
+                Save();
+            }
+        }
+
+        public void Load()
+        {
+            //call to saving system load
+            StartCoroutine(GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile));
+        }
+
+        public void Save()
+        {
+            //call to saving system save
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+
+        public void Delete()
+        {
+            GetComponent<SavingSystem>().Delete(defaultSaveFile);
+        }
+    }
+}
+
